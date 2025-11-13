df93a229d31f77b5484835a8264d788058732d22
przemekwrona <przemekwrona25@gmail.com>
2025-11-13 22:47:41 +0100

    feat: cache warsaw api response

--- src/client/warsaw-client/warsaw-client.module.ts
+++ src/client/warsaw-client/warsaw-client.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WarsawClientService } from './warsaw-client.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      ttl: 20 * 1000 // Cache expiration time in milliseconds
+    })
+  ],
   exports: [
     WarsawClientService
   ],
--- src/client/warsaw-client/warsaw-client.service.ts
+++ src/client/warsaw-client/warsaw-client.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { WarsawVehiclePositionResponse } from '../../generated/public-transport-api';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 
 export enum VehicleType {
   BUSES = 1,
@@ -10,6 +11,9 @@ export enum VehicleType {
 @Injectable()
 export class WarsawClientService {
   private POSITION_RESOURCE_KEY = 'f2e5503e927d-4ad3-9500-4ab9e55deb59';
+
+  constructor(@Inject(CACHE_MANAGER) private cache: Cache) {
+  }
 
   async getAllPositions(): Promise<WarsawVehiclePositionResponse> {
     return Promise.all([
@@ -36,8 +40,15 @@ export class WarsawClientService {
   async getWarsawPositions(
     vehicleType: VehicleType,
   ): Promise<WarsawVehiclePositionResponse> {
+    const cacheKey = `warsaw-positions:vehicle-type:${vehicleType}`;
+
+    const cached = await this.cache.get(cacheKey);
+    if (cached) return cached;
+
     const url: string = `https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=${this.POSITION_RESOURCE_KEY}&apikey=35d54d63-c436-4ef7-a98b-774bed55b393&type=${vehicleType}`;
     const response = await axios.get(url);
+
+    await this.cache.set(cacheKey, response.data);
     return response.data;
   }
 }
