199b9261e7a1cb81420aac1693cdd48941023b4d
przemekwrona <przemekwrona25@gmail.com>
2025-11-14 00:09:33 +0100

    feat: calculate velocity

--- src/delay/delay.module.ts
+++ src/delay/delay.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { DelayController } from './delay.controller';
 import { DelayService } from './delay.service';
 import { WarsawClientModule } from '../client/warsaw-client/warsaw-client.module';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    WarsawClientModule,
-    CacheModule.register({
-      ttl: 60 * 1000, // Cache expiration time in milliseconds
-    }),
+    WarsawClientModule
   ],
   controllers: [DelayController],
-  providers: [DelayService],
+  providers: [
+    DelayService
+  ],
 })
 export class DelayModule {}
--- src/delay/delay.service.ts
+++ src/delay/delay.service.ts
@@ -5,87 +5,42 @@ import {
   WarsawVehiclePositionResponse,
 } from '../generated/public-transport-api';
 import {
-  VehicleVelocity,
   VehicleVelocityResponse,
 } from '../generated/logisat-api';
-import moment, { Moment } from 'moment';
-import haversineDistance from 'haversine-distance';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { calculateVelocity } from './geo-utils';
+import {
+  CachedVehiclePositions,
+  CachedVehicles,
+  WarsawStreamPositionsService,
+} from '../client/warsaw-client/warsaw-stream-positions.service';
 
 @Injectable()
 export class DelayService {
   constructor(
     private warsawClientService: WarsawClientService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
-  ) {}
+    private warsawStreamPositionsService: WarsawStreamPositionsService) {
+  }
 
   async getLastPositions(): Promise<WarsawVehiclePositionResponse> {
     return this.warsawClientService.getAllPositions();
   }
 
   async getVelocity(): Promise<VehicleVelocityResponse> {
-    const lastPositions: WarsawVehiclePositionResponse =
-      await this.getLastPositions();
+    const cachedVehicles: CachedVehicles = await this.warsawStreamPositionsService.getStreamOfPositions();
 
-    const vehicleVelocities: VehicleVelocity[] = await Promise.all(
-      (lastPositions.result || []).map(async (currentPosition) => {
-        const historicalPosition =
-          await this.cacheManager.get<WarsawVehiclePosition>(
-            currentPosition.VehicleNumber,
-          );
+    const vehicles = (cachedVehicles.vehicles || []).map((vehicle: CachedVehiclePositions) => {
+      const lastPosition: WarsawVehiclePosition = vehicle.positions[0];
+      const previousPosition: WarsawVehiclePosition = vehicle.positions[1];
 
-        if (!historicalPosition) {
-          return {
-            vehicleNumber: currentPosition.VehicleNumber,
-            line: currentPosition.Lines || '',
-            velocity: 0,
-          };
-        }
+      const velocityMetersPerSecond = calculateVelocity(previousPosition, lastPosition);
 
-        const historicalTime = moment(historicalPosition.Time);
-        const currentTime = moment(currentPosition.Time);
+      return {
+        vehicleNumber: lastPosition.VehicleNumber,
+        line: lastPosition.Lines || '',
+        velocity: velocityMetersPerSecond,
+      };
+    });
 
-        const deltaTimeInSeconds = currentTime.diff(historicalTime, 'seconds');
-
-        if (deltaTimeInSeconds === 0) {
-          return {
-            vehicleNumber: currentPosition.VehicleNumber,
-            line: currentPosition.Lines || '',
-            velocity: 0,
-          };
-        }
-
-        const distanceInMeters = haversineDistance(
-          {
-            lat: historicalPosition.Lat || 0,
-            lon: historicalPosition.Lon || 0,
-          },
-          {
-            lat: currentPosition.Lat,
-            lon: currentPosition.Lon,
-          },
-        );
-
-        const velocityMetersPerSecond = Math.floor(distanceInMeters / deltaTimeInSeconds);
-
-        console.log(distanceInMeters + ' ' + deltaTimeInSeconds + ' ' + velocityMetersPerSecond);
-
-        return {
-          vehicleNumber: currentPosition.VehicleNumber,
-          line: currentPosition.Lines || '',
-          velocity: velocityMetersPerSecond,
-        };
-      }),
-    );
-
-    lastPositions.result?.forEach((vehicle: WarsawVehiclePosition) =>
-      this.cacheManager.set(vehicle.VehicleNumber, vehicle),
-    );
-
-    const response: VehicleVelocityResponse = {
-      vehicles: vehicleVelocities
-    };
-
-    return Promise.resolve(response);
+    return Promise.resolve({vehicles: vehicles} as VehicleVelocityResponse);
   }
 }
