895878b26d99c7257b406cc1db7d4a2008bb2e5b
przemekwrona <przemekwrona25@gmail.com>
2025-11-13 12:26:22 +0100

    feat: define logisat api

--- src/client/warsaw-client/warsaw-client.service.ts
+++ src/client/warsaw-client/warsaw-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { VehiclesGet200Response } from '../../generated/public-transport-api';
+import { WarsawVehiclePositionResponse } from '../../generated/public-transport-api';
 
 export enum VehicleType {
   BUSES = 1,
@@ -11,17 +11,17 @@ export enum VehicleType {
 export class WarsawClientService {
   private POSITION_RESOURCE_KEY = 'f2e5503e927d-4ad3-9500-4ab9e55deb59';
 
-  async getWarsawTramsPositions(): Promise<VehiclesGet200Response> {
+  async getWarsawTramsPositions(): Promise<WarsawVehiclePositionResponse> {
     return this.getWarsawPositions(VehicleType.TRAMS);
   }
 
-  async getWarsawBusesPositions(): Promise<VehiclesGet200Response> {
+  async getWarsawBusesPositions(): Promise<WarsawVehiclePositionResponse> {
     return this.getWarsawPositions(VehicleType.BUSES);
   }
 
   async getWarsawPositions(
     vehicleType: VehicleType,
-  ): Promise<VehiclesGet200Response> {
+  ): Promise<WarsawVehiclePositionResponse> {
     const url: string = `https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=${this.POSITION_RESOURCE_KEY}&apikey=35d54d63-c436-4ef7-a98b-774bed55b393&type=${vehicleType}`;
     const response = await axios.get(url);
     return response.data;
