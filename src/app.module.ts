86695fac2caed9c36aab7683a6941bbd10f40f39
przemekwrona <przemekwrona25@gmail.com>
2025-11-13 11:47:29 +0100

    feat: call Warsaw API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WarsawClientModule } from './client/warsaw-client/warsaw-client.module';
 
 @Module({
-  imports: [],
+  imports: [
+    WarsawClientModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { WarsawClientService } from './client/warsaw-client/warsaw-client.service';
 
 @Injectable()
 export class AppService {
+
+  constructor(private warsawClientService: WarsawClientService) {
+  }
+
   getHello(): string {
+    this.warsawClientService.getWarsawBusesPositions();
     return 'Hello World!';
   }
 }
