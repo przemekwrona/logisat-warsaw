1be472d1f20aa7019f6c140961e5f70a2795f5f2
przemekwrona <przemekwrona25@gmail.com>
2025-11-15 13:16:46 +0100

    feat: service to get users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UsersResponse } from '../generated/logisat-api';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/v1/users')
+export class UserController {
+
+  constructor(private userService: UserService) {
+  }
+
+  @Get()
+  async getUsers(): Promise<UsersResponse> {
+    return this.userService.getUsers();
+  }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersResponse } from '../generated/logisat-api';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  async getUsers(): Promise<UsersResponse> {
+    return Promise.resolve({} as UsersResponse);
+  }
+
+}
